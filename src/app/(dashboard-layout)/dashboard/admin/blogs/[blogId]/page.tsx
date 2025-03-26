568b7e54553fb853975e217d785149a225cb6657
Arnab Gupta <arnabgupta1111@gmail.com>
2025-03-26 20:03:19 +0600

    Add Update blog form and show the blog information

--- src/app/(dashboard-layout)/dashboard/admin/blogs/[blogId]/page.tsx
+++ src/app/(dashboard-layout)/dashboard/admin/blogs/[blogId]/page.tsx
@@ -1,14 +1,20 @@
+import UpdateBlogForm from "@/components/modules/dashboard/admin/ManageBlogs/UpdateBlogForm";
 import Spinner from "@/components/shared/Loader/Spinner/Spinner";
 import { getBlogDetails } from "@/services/blog";
-
+import { Suspense } from "react";
 
 const UpdateListingPage = async ({ params }: { params: Promise<{ blogId: string }> }) => {
   const { blogId } = await params;
 
   const res = await getBlogDetails(blogId);
   const { data } = res;
-  console.log(data);
-  return <div>{/* <UpdateListingForm listingDetails={data} /> */}</div>;
+  return (
+    <div>
+      <Suspense fallback={<Spinner />}>
+        <UpdateBlogForm blogs={data} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default UpdateListingPage;
